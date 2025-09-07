0cdc562873a4c2145c7ce1548bcdc17f308344e7
samsonkimani <samsonkimani43@gmail.com>
2025-09-07 22:30:18 +0300

    updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,17 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import PropertyCard from "@/components/property/PropertyCard";
 
+
+interface Property {
+  id: string;
+  title: string;
+  image: string;
+  price: number;
+  location: string;
+}
+
 export default function Home() {
-  const [properties, setProperties] = useState([]);
+  const [properties, setProperties] = useState<Property[]>([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
